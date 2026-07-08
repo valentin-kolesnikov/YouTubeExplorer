9fe2124c7be63d93293b9aaa75276c34c420682e
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2026-07-08 21:06:49 +0300

    One Video Explorer collects logs

--- Modes/YouTubeOneVideoExplorer.py
+++ Modes/YouTubeOneVideoExplorer.py
@@ -6,6 +6,10 @@ from Patterns.collectingStats import collect_stats
 
 from Patterns.check_connection import internet_available
 
+from Patterns.HistoryLogs import HistorySessions
+
+from Patterns.save_history import log, log_error, clear
+
 from SecondFunctions.output import output_videos
 
 from asyncio import run
@@ -15,19 +19,31 @@ from asyncio import run
 
 
 def launcherInfo(youtube):
+    history = HistorySessions("One Video Info")
+
+
     video_id = youtube_id_finder()
+    log(history, "ENTER_VIDEO_LINK", video_link="https://www.youtube.com/watch?v=" + video_id)
+
 
     internet_available()
+    log(history, "CHECK_INTERNET")
+
     
     statrequest, exc = collect_stats(youtube, [video_id])
     if exc:
-        print("\033[H\033[J", end="")
+        log_error(history, statrequest, exc)
+        clear()
+
         return
-    
+
     results = run(ryd([video_id]))
 
-    print("\033[H\033[J", end="")
+    log(history, "COLLECT_ONE_VIDEO_INFO", status="SUCCESS")
+    clear()
 
     output_videos(results, statrequest, one_video_info=True)
+    log(history, "OUTPUT_ONE_VIDEO_INFO", status="SUCCESS")
 
     input("\n\nPress Enter to return...")
+    log(history, "EXIT")
