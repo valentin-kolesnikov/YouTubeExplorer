b8425f470bc1db16f088d018ec3c5f03ae858292
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2026-07-08 16:30:04 +0300

    The error has been fixed

--- PlaylistExplorer_InputData/Videos_of_Playlists.py
+++ PlaylistExplorer_InputData/Videos_of_Playlists.py
@@ -1,39 +1,44 @@
-from Patterns.collectingStats import collect_stats
-
-from Patterns.asyncRYD import ryd
-
 from SecondFunctions.output import output_videos
 
 from ForthFunctions.collecting_info import collect_videos_of_playlist
 
 from InputData.PlaylistExplorer import playlist_URL_extract
 
+from Patterns.collectingStats import collect_stats
+
+from Patterns.save_history import log, log_error, clear
+
+from Patterns.asyncRYD import ryd
+
 from asyncio import run
 
 
 
 
-
-
-def videos_of_playlists(youtube):
+def videos_of_playlists(history, youtube):
     playlist_URL = playlist_URL_extract()
+    log(history, "ENTER_PLAYLIST_LINK", playlist_link="https://www.youtube.com/playlist?list=" + playlist_URL)
 
     video_ids, exc = collect_videos_of_playlist(youtube, playlist_URL)
     if exc:
-        print("\033[H\033[J", end="")
+        log_error(history, video_ids, exc)
+        clear()
         return
     
     results = run(ryd(video_ids))
 
     statrequest, exc = collect_stats(youtube, video_ids)
     if exc:
-        print("\033[H\033[J", end="")
+        log_error(history, statrequest, exc)
+        clear()
         return
-
-    print("\033[H\033[J", end="")
+    
+    log(history, "COLLECT_VIDEOS", status="SUCCESS")
+    clear()
 
     print(f"https://www.youtube.com/playlist?list={playlist_URL}\n")
-    output_videos(results, statrequest)
+    output_videos(results, statrequest, one_video_info=False)
+    log(history, "OUTPUT_VIDEOS")
 
     input("\nPress Enter to return...")
     return
