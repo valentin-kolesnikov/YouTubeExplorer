6b24fc8031fcbaa48dc7817f2646689836abe785
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2026-07-11 02:10:35 +0300

    Refactor internet availability check and improve error handling

--- Patterns/check_connection.py
+++ Patterns/check_connection.py
@@ -2,18 +2,29 @@ from Patterns.errors import WinError
 
 from urllib.request import urlopen
 
+from urllib.error import URLError
 
 
 
+TEST_URL = (
+    "http://clients3.google.com/generate_204",
+    "https://www.cloudflare.com"
+    "https://www.microsoft.com"
+)
 
 
 def internet_available():
     while True:
         try:
-            urlopen("http://clients3.google.com/generate_204", timeout=3)
-            return True
+            urlopen(TEST_URL)
+            return False
 
         except OSError as exc:
             
             if WinError(exc):
                 continue
+
+        except URLError as exc:
+            print("The check server is likely unavailable.")
+            if isinstance(exc.reason, OSError):
+                WinError(exc.reason)
